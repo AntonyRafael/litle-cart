c9ec4002873499341d3025bcad528659ec81c387
Antony Rafael <depaulacostaantony@gmail.com>
2022-07-15 08:32:11 -0300

    fix css reset

--- src/layout/styles.ts
+++ src/layout/styles.ts
@@ -4,7 +4,7 @@ export const AppLayoutStyles = styled.main`
   background: #fefefe;
   border-radius: 8px;
   width: 100%;
-  max-width: 450px;
+  max-width: 80vw;
   box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
 
 `;
